160dc927ad5ec7aae5ded2f93486c6c4eb0edf2e
Lolli Di <290romchik@gmail.com>
2021-11-27 09:41:47 +0300

    Готово

--- serverKanali/serverKanali.cpp
+++ serverKanali/serverKanali.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#define SIZE_BUFFER 140
 
 int main()
 {
@@ -9,10 +10,11 @@ int main()
     HANDLE hNamedPipe; //дескриптор канала
     LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";//имя канала
     BOOL Connected;
-    DWORD sizeBuffer = 100;
+    DWORD sizeBuffer = SIZE_BUFFER;
     DWORD actualReaden;
     BOOL SuccessRead;
-    LPWSTR buffer;
+    LPWSTR buffer = (CHAR*)calloc(sizeBuffer, sizeof(CHAR));
+    char message[SIZE_BUFFER];
     while (TRUE)
     {
         hNamedPipe = CreateNamedPipe(lpszPipeName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, 512, 512, INFINITE, NULL); //Инициалиция канала
@@ -29,26 +31,38 @@ int main()
                 printf("\nКлиент подключился\n");
                 i = 0;
             }
-            
-            buffer = (CHAR*)calloc(sizeBuffer, sizeof(CHAR));
            
             SuccessRead = ReadFile(hNamedPipe, buffer, sizeBuffer, &actualReaden, NULL);
-            printf(buffer);
-            if (!SuccessRead)
+            
+            while (!SuccessRead)
             {
-                if (i > 1)
+                if (i > 100)
                 {
                     printf("\nКлиент отключился!\n");
-                    i = -2;
+                    i = -1;
+                    break;
                 }
                 i++;
+                SuccessRead = ReadFile(hNamedPipe, buffer, sizeBuffer, &actualReaden, NULL);
             }
-            else
+            if (SuccessRead)
             {
-                i = 0;
+                printf("Клиент пишет: ");
+                printf(buffer);
+                printf("\n");
+
+                printf("Введите сообщение для клиента:\n");
+                gets(message);
+                buffer = &message;
+                WriteFile(hNamedPipe, buffer, sizeBuffer, &actualReaden, NULL);
             }
-            
             CloseHandle(hNamedPipe);
+        }
+        else 
+        {
+            buffer = "";
+            printf("\nКлиент отключился!\n");
+            i = -1;
         }
     }
     printf("\nСервер отключен!\n");
