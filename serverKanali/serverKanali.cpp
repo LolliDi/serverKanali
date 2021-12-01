543b45dd92440f14b7610745b98c16fdc7380e1a
Lolli Di <290romchik@gmail.com>
2021-12-01 23:06:56 +0300

    клиент выводит только первую цифру в ответе

--- serverKanali/serverKanali.cpp
+++ serverKanali/serverKanali.cpp
@@ -1,6 +1,47 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
+#include <stdio.h>
 #define SIZE_BUFFER 140
+
+double perevod(char* c)
+
+{
+    double a;
+    WORD prov = 0;
+    WORD i = 0;
+    while (c[i] != '\0') //пока не наткнемся на конец - ищем точку или запятую, чтобы отделить целую часть от дробной
+    {
+        if (c[i] == ',' || c[i] == '.')
+        {
+            prov = 1; //если находим, то прерываемся и записываем в значение 1, чтобы потом посмотреть дробь ли это
+            break;
+        }
+        i++;
+    }
+    if (prov == 1) //если дробь
+    {
+        char* b = strtok(c, ",."); //целая часть отделяется
+        a = atoi(b); //переводим целую часть из строки в число
+        b = strtok(NULL, ",."); //дробная часть
+        if (b != NULL)
+        {
+            double aa = atoi(b); //переводим дробную часть из строки в число
+            WORD kol = 1;
+            while (*b != '\0') //смотрим какая степень у дроби и делим на 10 каждый раз, чтобы целое число стало дробным
+            {
+                b++;
+                aa = aa / 10;
+            }
+            if (a >= 0)
+                a = a + aa; //прибавляем дробную часть к целой
+            else
+                a = a - aa;
+        }
+    }
+    else
+        a = atoi(c);//если число целое, то просто присваиваем
+    return a;
+}
 
 int main()
 {
@@ -14,7 +55,7 @@ int main()
     DWORD actualReaden;
     BOOL SuccessRead;
     LPWSTR buffer = (CHAR*)calloc(sizeBuffer, sizeof(CHAR));
-    char message[SIZE_BUFFER];
+    char* message = calloc(sizeBuffer, sizeof(char));
     while (TRUE)
     {
         hNamedPipe = CreateNamedPipe(lpszPipeName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, 512, 512, INFINITE, NULL); //Инициалиция канала
@@ -36,31 +77,63 @@ int main()
             
             while (!SuccessRead)
             {
-                if (i > 100)
+                if (i == 100)
                 {
                     printf("\nКлиент отключился!\n");
-                    i = -1;
                     break;
                 }
                 i++;
                 SuccessRead = ReadFile(hNamedPipe, buffer, sizeBuffer, &actualReaden, NULL);
             }
+            i = -1;
             if (SuccessRead)
             {
-                printf("Клиент пишет: ");
                 printf(buffer);
-                printf("\n");
-
-                printf("Введите сообщение для клиента:\n");
-                gets(message);
-                buffer = &message;
+                char* st = malloc(sizeof(char) * 100);
+                char* stt = st;
+                char* c = buffer;
+                while (*c != '\0') //перезаписываем lpswtr в char* для разбивки + проверка на корректность данных
+                {
+                    c--;
+                    char cmin = *c; //предыдущий символ
+                    c++;
+                    c++;
+                    char cmax = *c; //следующий символ
+                    c--;
+                    *st = *c;
+                    if (*st == '\r')
+                    {
+                        *st = '\n';
+                        c++;
+                    }//      число                    или пробел   или перенос строки   или ;     или минус      в начале строки    и не после чисел               и перед числом                      или , или .               но только между числами
+                    if ((*st >= '0' && *st <= '9') || *st == ' ' || *st == '\n' || *st == ';' || (*st == '-' && (st == stt || (!(cmin >= '0' && cmin <= '9') && (cmax >= '0' && cmax <= '9')))) || ((*st == ',' || *st == '.') && !(cmin == ',' || cmin == '.' || cmin == '-') && (cmax >= '0' && cmax <= '9')))
+                    {
+                        st++;
+                        c++;
+                    }
+                    else
+                    {
+                        buffer = L"Неверный формат входных данных!";
+                        break;
+                    }
+                }
+                if (st != stt)
+                {
+                    *st = L'\0';
+                    double kv = perevod(stt);
+                    kv = kv * kv;
+                    char* op = malloc(sizeof(wchar_t) * 150);
+                    sprintf(op, L"%f", kv); //записываем данные в чар
+                    wchar_t iii[150];
+                    MultiByteToWideChar(CP_ACP, 0, op, -1, iii, SIZE_BUFFER); //перевод из чар*
+                    buffer = iii;
+                }
                 WriteFile(hNamedPipe, buffer, sizeBuffer, &actualReaden, NULL);
+                buffer = (CHAR*)calloc(sizeBuffer, sizeof(CHAR));
             }
             CloseHandle(hNamedPipe);
         }
-        else 
-        {
-            buffer = "";
+        else {
             printf("\nКлиент отключился!\n");
             i = -1;
         }
